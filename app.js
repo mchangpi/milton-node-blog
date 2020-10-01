e8de87b4b98a76c18cfa973b2f28f3dc3a23382d
Milton Chang <mchangpi@gmail.com>
2020-10-01 12:22:49 +0800

    364. REST APIs, Clients & CORS Errors

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, resp, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
