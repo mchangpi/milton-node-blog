fc2187f268e2ecf5c19b3dd9a604ee7db53daa78
Milton Chang <mchangpi@gmail.com>
2020-10-03 08:23:01 +0800

    408. Establishing a Connection From the Client

--- client/src/pages/Feed/Feed.js
+++ client/src/pages/Feed/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from "react";
+import openSocket from "socket.io-client";
 
 import Post from "../../components/Feed/Post/Post";
 import Button from "../../components/Button/Button";
@@ -43,6 +44,7 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
+    openSocket(process.env.REACT_APP_SERVER);
   }
 
   loadPosts = (direction) => {
