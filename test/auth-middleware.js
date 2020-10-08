38253bbf0141c075549d3350c869e3970f5f64a2
Milton Chang <mchangpi@gmail.com>
2020-10-08 23:40:30 +0800

    469. Using Sinon Stubs

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,5 +1,7 @@
 const { expect } = require("chai");
 const authMiddleware = require("../middleware/is-auth");
+const jwt = require("jsonwebtoken");
+const sinon = require("sinon");
 
 describe("Auth middleware: ", () => {
   it("should throw an error if no authorization header is present", () => {
@@ -17,6 +19,31 @@ describe("Auth middleware: ", () => {
     const req = {
       get: () => {
         return "bearer ";
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should yield an userId after decoding the token.", () => {
+    const req = {
+      get: () => {
+        return "bearer blablablabla ";
+      },
+    };
+    sinon.stub(jwt, "verify");
+    jwt.verify.returns({ userId: "abc" });
+
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property("userId", "abc");
+    expect(jwt.verify.called).to.be.true;
+
+    jwt.verify.restore();
+  });
+
+  it("should throw an error if the token cannot be verified", () => {
+    const req = {
+      get: () => {
+        return "bearer xyz";
       },
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
