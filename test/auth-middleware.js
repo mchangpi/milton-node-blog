5459449634d387d9e5d4c95073f16c1ccfedfc14
Milton Chang <mchangpi@gmail.com>
2020-10-08 22:09:07 +0800

    466. Testing the Auth Middleware

---
+++ test/auth-middleware.js
@@ -0,0 +1,13 @@
+const { expect } = require("chai");
+const authMiddleware = require("../middleware/is-auth");
+
+it("should throw an error if no authorization header is present", () => {
+  const req = {
+    get: () => {
+      return null;
+    },
+  };
+  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+    "Not authenticated"
+  );
+});
