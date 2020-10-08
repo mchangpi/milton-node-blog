85ddde01c892e7a6c8e99cc5ed88472fe1762546
Milton Chang <mchangpi@gmail.com>
2020-10-08 22:30:02 +0800

    467. Organization Multiple Tests

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,13 +1,24 @@
 const { expect } = require("chai");
 const authMiddleware = require("../middleware/is-auth");
 
-it("should throw an error if no authorization header is present", () => {
-  const req = {
-    get: () => {
-      return null;
-    },
-  };
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-    "Not authenticated"
-  );
+describe("Auth middleware: ", () => {
+  it("should throw an error if no authorization header is present", () => {
+    const req = {
+      get: () => {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated"
+    );
+  });
+
+  it("should throw an error if the authorization header is only one string", () => {
+    const req = {
+      get: () => {
+        return "bearer ";
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
